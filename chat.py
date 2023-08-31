b53dd587c8e8bd333f258a51b220cb04f7440e14
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2023-08-31 18:12:40 +0300

    chank size 256

--- chat.py
+++ chat.py
@@ -93,7 +93,8 @@ class GPT():
 
     source_chunks = []
     #splitter = CharacterTextSplitter(separator="\n", chunk_size=1524, chunk_overlap=0)
-    splitter = CharacterTextSplitter(separator="<", chunk_size=1024, chunk_overlap=300)
+    #splitter = CharacterTextSplitter(separator="<", chunk_size=1024, chunk_overlap=300)
+    splitter = CharacterTextSplitter(separator="<", chunk_size=256, chunk_overlap=100)
 
     for chunk in splitter.split_text(data):
       source_chunks.append(Document(page_content=chunk, metadata={}))
